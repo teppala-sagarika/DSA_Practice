15b6dc8479bc88cb45844c96d24b1b3c8d1369f6
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-03 21:55:06 +0530

    Binary addition

--- BinaryNumberSystem/binary.cpp
+++ BinaryNumberSystem/binary.cpp
@@ -29,6 +29,45 @@ void decToBin(int decNo){
     cout<<"binary no.="<<binNo<<endl;
 }
 
+//adding 2 binary no.s
+void addBin(int b1, int b2)
+{
+    int c = 0;
+    int ans = 0;
+    int pow = 1;
+
+    while (b1 > 0 || b2 > 0)
+    {
+        int r1 = b1 % 10;
+        int r2 = b2 % 10;
+
+        b1 /= 10;
+        b2 /= 10;
+
+        int s = r1 + r2 + c;
+
+        if (s == 0 || s == 1) {
+            ans += pow * s;
+            c = 0;
+        }
+        else if (s == 2) {
+            ans += pow * 0;
+            c = 1;
+        }
+        else { // s == 3
+            ans += pow * 1;
+            c = 1;
+        }
+        pow *= 10;   
+    }
+
+    if (c == 1) {
+        ans += pow; // add final carry 
+    }
+
+    cout << "sum = " << ans << endl;
+}
+
 int main(){
     int b,d;
     cout<<"enter a binary no.="<<endl;
@@ -39,4 +78,8 @@ int main(){
     cin>>d;
     cout<<"\nDecimal to binary=";
     decToBin(d);
+    int b1,b2;
+    cout<<"\nprovide 2 binary no.s=";
+    cin>>b1>>b2;
+    addBin(b1,b2);
 }
