57eddbc9d2be39d847f75c9627380646e73dc917
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-11 20:46:00 +0530

    arrays

---
+++ Arrays/arr1.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printArr(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<",";
+    }
+    cout<<endl;
+}
+int main(){
+    int marks[5]={1,2,3,4,5};
+    cout<<sizeof(marks)<<endl;//20 //each 4 bytes
+    int len=sizeof(marks)/sizeof(int);
+    cout<<"Length of array="<<len<<endl;//5
+    //indexing
+    cout<<marks[3]<<endl;//4 // indexing starts from 0
+    //printing elements of array
+    for(int i=0;i<len;i++){
+        cout<<marks[i]<<" ";
+    }
+    cout<<"\n";
+    int n;
+    cout<<"enter length of array= ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter array elements= ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"array elements are ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<'\n';
+    //largest and smallest elements of array 
+    int arr1[]={7,2,4,19,6};
+    int x=sizeof(arr1)/sizeof(int);
+    int L=arr1[0];
+    int S=arr1[0];
+    for(int i=0;i<x;i++){
+        if(arr1[i]>L){
+            L=arr1[i];
+        }
+        if(arr1[i]<S){
+            S=arr1[i];
+        }
+    } 
+    cout<<"Largest no. in array="<<L<<endl;
+    cout<<"Smallest no. in array="<<S<<endl;
+    //arrays are passed by reference
+    int a[]={2,4,6,8,10};
+    cout<<*a<<endl;//a[0]
+    cout<<*(a+1)<<endl;//a[1]
+    cout<<*(a+2)<<endl;//a[2]
+    printArr(a,5);
+}
