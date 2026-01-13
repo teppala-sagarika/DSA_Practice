bf82635124118a05b446d32b86fee000d295df13
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-13 23:17:59 +0530

    bubble sort optimized

--- SortingAlgos/bubblesort.cpp
+++ SortingAlgos/bubblesort.cpp
@@ -11,10 +11,15 @@ int main(){
     cout<<"\n";
     //bubble sort
     for(int i=0;i<n-1;i++){
+        bool isSwap=false;
         for(int j=0;j<n-1-i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                isSwap=true;
             }
+        }
+        if(!isSwap){
+            break;
         }
     }
     cout<<"After Sorting"<<endl;
