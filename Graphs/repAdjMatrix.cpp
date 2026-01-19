72d2cd68b79767775339f14e7659fdf7a391a2a1
Teppala Sagarika <sagarikateppala@gmail.com>
2026-01-19 20:30:24 +0530

    Graph representation -Adjacency Matrix

---
+++ Graphs/repAdjMatrix.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;//n = nodes , m = edges
+    cin>>n>>m;
+    int adj[n+1][n+1];
+    for(int i=0;i<m;i++){
+        int u,v;//nodes
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    return 0;
+}
